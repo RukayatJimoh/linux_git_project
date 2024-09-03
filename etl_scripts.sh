f653ec3b1bd781b86a657bf37a405dde4c10e185
Rukayat Jimoh <rukayatjimoh@Rukayats-MacBook-Pro.local>
2024-09-03 22:05:36 +0200

    Add Bash script for ETL process to download and save CSV file

---
+++ etl_scripts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Defining variables
+RAW_DIR="./raw"
+CSV_URL="https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv"
+FILE_NAME="annual-enterprise-survey-2023-financial-year-provisional.csv"
+TRANSFORMED_PATH="transformed"
+TRANSFORMED_FILE="2023_year_finance.csv"
+GOLD_PATH="gold"
+GOLD_FILE="2023_year_finance_gold.csv"
+
+# Creating raw directory
+if [ ! -d "$RAW_DIR" ]; then
+    echo "Creating directory $RAW_DIR ..."
+    mkdir "$RAW_DIR"
+fi
+
+# Download the CSV file
+echo "Downloading $FILE_NAME from $CSV_URL..."
+curl -o "$RAW_DIR/$FILE_NAME" "$CSV_URL"
+
+# Check if file was successfully downloaded
+if [ -f "$RAW_DIR/$FILE_NAME" ]; then
+    echo "File $FILE_NAME has been saved into the directory $RAW_DIR"
+else
+    echo "Failed to download $FILE_NAME."
+fi
